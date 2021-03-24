314e1ba3fa85f710987d3731dc57cfb5a4f7599d
Joey Dang <jodang@ucsd.edu>
2021-03-24 14:31:45 -0700

    Add routing to pages

--- responder/src/index.js
+++ responder/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
 import './index.scss';
 
+
+import MainPage from './pages/MainPage';
+import LandingPage from './pages/LandingPage';
+
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <React.StrictMode>
+    <Router>
+      <Switch>
+        <Route path="/main" component={MainPage} />
+        <Route path="/" component={LandingPage} />
+      </Switch>
+    </Router>
+  </React.StrictMode>,
   document.getElementById('root')
 );
