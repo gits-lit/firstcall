0c6b5ea17d4a67802be3bcc836eab14e3dce9b35
Jennifer <32044772+Froggerson@users.noreply.github.com>
2021-03-24 14:07:08 -0700

    Add 3D model to map

--- caller/src/components/Map/index.jsx
+++ caller/src/components/Map/index.jsx
@@ -7,14 +7,57 @@ const Map = ReactMapboxGl({
     'pk.eyJ1IjoiZW1pbmd1eWVuIiwiYSI6ImNrOGI2ZjRyODA1aHEzZG93cmFxaHR5d2IifQ.x8v_uFbdBanYgRtoKCGIOw'
 });
 
+/**
+ * Loads the 3D model
+**/
+const loadLocations = (map) => {
+  map.addLayer({
+   id: 'bananya',
+   type: 'custom',
+   renderingMode: '3d',
+   onAdd: function (map, mbxContext) {
+
+     window.tb = new Threebox(
+       map,
+       mbxContext,
+       { defaultLights: true, enableDraggingObjects: true, enableSelectingObjects:true }
+     );
+
+     var options = {
+       obj: '/bananya_birbo/scene.gltf',
+       type: 'gltf',
+       scale: 40,
+       units: 'meters',
+       anchor: 'center',
+       rotation: { x: 90, y: 180, z: 0 } //default rotation
+     }
+
+     window.tb.loadObj(options, function (model) {
+       const cat = model.setCoords([-117.06651266267941, 32.76570649214452]);
+       console.log("Nya~ nya~ nya~ pls end me");
+       window.tb.add(cat);
+     })
+
+   },
+   render: function (gl, matrix) {
+     window.tb.update();
+   }
+ });
+}
+
+/**
+ * The actual component
+**/
 const MapComponent = (props) => {
   const leftMargin = props.sideBarVis ? '15vw' : '0';
-  console.log(props.sideBarVis);
+  console.log("Loading Map...");
 
   let locations = {};
   const mapLoad = map => {
-    window.map = map;
-    setTimeout(2000);
+    setTimeout(() => {
+      window.map = map;
+      loadLocations(map);
+    }, 2000);
   };
 
   return (
