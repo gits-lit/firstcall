4806cdacf8bd820db3eb42393d6751de59851fac
Joey Dang <jodang@ucsd.edu>
2021-03-24 19:08:07 -0700

    Add placeholder and size down font

--- responder/src/components/NavBar/SearchBar/index.jsx
+++ responder/src/components/NavBar/SearchBar/index.jsx
@@ -5,15 +5,16 @@ import search from '.../../../public/search.svg'
 import location from '.../../../public/location.svg'
 
 const SearchBar = (props) => {
-  const [address, setAddress] = useState('8036 W 3rd St, Los Angeles, CA 90048');
+  const [address, setAddress] = useState('');
 
   return (
     <div className="search-bar">
       <img className="location" src={location} alt="location pin"/>
       <input
+        onChange={e => setAddress(e.target.value)}
+        placeholder='8036 W 3rd St, Los Angeles, CA 90048'
         type="text"
         value={address}
-        onChange={e => setAddress(e.target.value)}
         />
       <img className = "search" src={search} alt="search"/>
     </div>
