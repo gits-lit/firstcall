f4dc7ec8f110c62e9529ea9f815fc0f79a460b9d
Joey Dang <jodang@ucsd.edu>
2021-03-24 18:45:46 -0700

    Finish sidebar

---
+++ responder/src/components/SideBar/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import log from '../../assets/log.svg';
+import dial from '../../assets/dial.svg';
+import gear from '../../assets/gear.svg';
+import stats from '../../assets/stats.svg';
+
+import './style.scss'
+
+const SideBar = (props) => {
+  return (
+    <div className="side-bar">
+    <div className="tabs">
+      <div
+        className={props.click === 'log' ? 'tab selected' : 'tab'}
+        onClick={() => {
+          props.setClick('log');
+        }}
+      >
+        <div className="background"></div>
+        <img src={log} alt="logo" className="log" />
+      </div>
+      <div
+        className={props.click === 'dial' ? 'tab selected' : 'tab'}
+        onClick={() => {
+          props.setClick('dial');
+        }}
+      >
+        <div className="background"></div>
+        <img src={dial} alt="logo" className="dial" />
+      </div>
+      <div
+        className={props.click === 'stats' ? 'tab selected' : 'tab'}
+        onClick={() => {
+          props.setClick('stats');
+        }}
+      >
+        <div className="background"></div>
+        <img src={stats} alt="logo" className="stats" />
+      </div>
+    </div>
+    <div className="gear">
+      <img src={gear} alt="logo" />
+    </div>
+  </div>
+  );
+};
+
+export default SideBar;
