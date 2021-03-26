247cfa782009a5431fd84c8877c7e95f7cadd41d
Joey Dang <jodang@ucsd.edu>
2021-03-26 14:48:08 -0700

    Finish header

--- responder/src/components/DeployHelp/index.jsx
+++ responder/src/components/DeployHelp/index.jsx
@@ -1,9 +1,34 @@
+import React, {useState} from 'react';
+import SearchBar from '../CallCenter/SearchBar';
 import './style.scss';
 
 const DeployHelp = () => {
+  const [searchUnit, setSearchUnit] = useState('');
+
   return (
     <div className="deploy-help">
-      <h1>Deploy Help</h1>
+      <div className="route-options">
+        <h1 className="deploy-header">Deploy Help</h1>
+        <div className="option-num">
+          <div className="requested-option">
+            <div className="requested-circle"></div>
+            <div className="option-description"><h1>{2}</h1><span>Requested</span></div>
+          </div>
+          <div className="option">
+            <div className="enroute-circle"></div>
+            <div className="option-description"><h1>{2}</h1><span>Enroute</span> </div>
+          </div>
+          <div className="option">
+            <div className="onsight-circle"></div>
+            <div className="option-description"><h1>{2}</h1><span>On-Site</span></div>
+          </div>
+        </div>
+      </div>
+      <SearchBar name="for Unit"
+        setSearchId={(searchUnitInput) => {
+          setSearchUnit(searchUnitInput);
+        }}
+      />
     </div>
   )
 }
