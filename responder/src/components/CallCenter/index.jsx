3be7d9d94b14046321eabe3540a2805b0ee7968a
eminguyen <emily@computer.org>
2021-03-25 23:05:29 -0700

    Combine everything

--- responder/src/components/CallCenter/index.jsx
+++ responder/src/components/CallCenter/index.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Dropdown, Menu, Switch } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
+import anime from 'animejs';
 
 import './style.scss';
 
@@ -9,6 +10,20 @@ import Log from './Log';
 import SearchBar from './SearchBar';
 
 const CallCenter = (props) => {
+const animationRef = React.useRef(null);
+  useEffect(() => {
+    animationRef.current = anime({
+      targets: '.log-status',
+      opacity: '1',
+      delay: anime.stagger(200, {start: 0}),
+      translateY: [{value: 0, easing: 'easeInCubic'}, {value: -25, easing: 'easeOutCubic'}],
+      direction: 'alternate',
+      easing: 'easeInExpo',
+      duration: 200 * props.data.length,
+      loop: false,
+      autoplay: true
+  });   
+  }, [props.data])
 
   return (
     <div className="call-center">
@@ -27,17 +42,17 @@ const CallCenter = (props) => {
         <CaseFilter />
       </div>
       <div className="logs">
-      {props.data.map(user => {
-          console.log(user);
-          const date = new Date(user.startDate)
-          const minutes = date.getMinutes();
-          const hours = date.getHours() % 12;
-          const ampm = date.getHours() > 11 ? 'PM': 'AM';
-          return (
-            <Log status={user.status} caseId={user.caseId}
-            time={`${hours}:${minutes} ${ampm}`} location={user.address}/>
-          )
-        })}
+        {props.data.map(user => {
+            console.log(user);
+            const date = new Date(user.startDate)
+            const minutes = date.getMinutes();
+            const hours = date.getHours() % 12;
+            const ampm = date.getHours() > 11 ? 'PM': 'AM';
+            return (
+              <Log status={user.status} caseId={user.caseId}
+              time={`${hours}:${minutes} ${ampm}`} location={user.address}/>
+            )
+          })}
       </div>
     </div>
   )
