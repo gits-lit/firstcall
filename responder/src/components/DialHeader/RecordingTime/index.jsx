d66fe7cbeae9eb4d85767a73554e12599ea277cd
Jennifer <32044772+Froggerson@users.noreply.github.com>
2021-03-27 01:40:57 -0700

    components sorta work for dial header

--- responder/src/components/DialHeader/RecordingTime/index.jsx
+++ responder/src/components/DialHeader/RecordingTime/index.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react';
+import recording from '.../../../public/recording.svg'
 
 import './style.scss';
 
 const RecordingTime = (props) => {
+  const [minutes, setMinutes] = useState("00");
+  const [seconds, setSeconds] = useState("00");
+
   return (
     <div className="recording-time">
+      <img className="recording-icon" src={recording} alt="recording icon"/>
+      <h2>{minutes}:{seconds}</h2>
     </div>
   )
 }
