82d77b264371d7085b0b0d1f89f84ed08be566d0
Joey Dang <jodang@ucsd.edu>
2021-03-27 01:21:19 -0700

    Add route logs

--- responder/src/components/DeployHelp/index.jsx
+++ responder/src/components/DeployHelp/index.jsx
@@ -1,38 +1,65 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 import HelpCarousel from './HelpCarousel';
+import RouteLog from './RouteLog';
 import SearchBar from '../CallCenter/SearchBar';
 
 import './style.scss';
 
-const DeployHelp = () => {
+const DeployHelp = (props) => {
   const [searchUnit, setSearchUnit] = useState('');
-  const [unitType, setUnitType] = useState('Police');
+  const [requestedCount, setRequestedCount] = useState(0);
+  const [enrouteCount, setEnrouteCount] = useState(0);
+  const [onsightCount, setOnsightCount] = useState(0);
 
-  //temp carousel
-  const temp = [{
-    name:'Los Angeles Police Department',
-    address:'2111 E 1st St, Los Angeles, CA 90033',
+  useEffect(() => {
+    let requestedStatus = 0;
+    let enrouteStatus = 0;
+    let onsightStatus = 0;
+
+    // for (let i = 0; i < props.data.length; i++) {
+    //   if (props.data[i].status === 'Requested') {
+    //     requestedStatus += 1;
+    //   } else if (props.data[i].status === 'Enroute') {
+    //     enrouteStatus += 1;
+    //   } else {
+    //     onsightStatus += 1;
+    //   }
+    // }
+
+    setRequestedCount(requestedStatus);
+    setEnrouteCount(enrouteStatus);
+    setOnsightCount(onsightStatus);
+  }, [props.responderData]);
+
+  //temp route log
+  const tempRequested = [{
+    status:'Requested',
+    unitId:'DPQO32',
     type:'Police',
-    'unit-img':'http://placekitten.com/200/300'
+    eta:'-:--PM',
+    dispatch:'2:12PM'
   },
   {
-    name:'Los Angeles Police Department 2',
-    address:'2111 E 1st St, Los Angeles, CA 90033',
+    status:'Requested',
+    unitId:'DPQO32',
     type:'Ambulance',
-    'unit-img':'http://placekitten.com/200/300'
+    eta:'2:14PM',
+    dispatch:'2:11PM'
   },
   {
-    name:'Los 1 Police Department',
-    address:'2111 E 1st St, Los Angeles, CA 90033',
+    status:'Requested',
+    unitId:'DPQO32',
     type:'Police',
-    'unit-img':'http://placekitten.com/200/300'
+    eta:'2:14',
+    dispatch:'2:11PM'
   },
   {
-    name:'Ls2s Police Department',
-    address:'2111 E 1st St, Los Angeles, CA 90033',
+    status:'Requested',
+    unitId:'DPQO32',
     type:'Police',
-    'unit-img':'http://placekitten.com/200/300'
+    eta:'Arrived',
+    dispatch:'2:12PM'
   }]
 
   return (
@@ -59,7 +86,57 @@ const DeployHelp = () => {
           setSearchUnit(searchUnitInput);
         }}
       />
-      <HelpCarousel unit={temp} searchValue={searchUnit}/>
+      <HelpCarousel unit={props.responderData} searchValue={searchUnit}/>
+      <div className="route-logs">
+        <h1 className="route-status-header">Requested</h1>
+        { tempRequested.map(route => {
+          const date = new Date(route.ETA)
+          const minutes = date.getMinutes();
+          const hours = date.getHours() % 12;
+          const ampm = date.getHours() > 11 ? 'PM': 'AM';
+          const dateDispatch = new Date(route.ETA)
+          const minutesDispatch = dateDispatch.getMinutes();
+          const hoursDispatch = dateDispatch.getHours() % 12;
+          const ampmDispatch = dateDispatch.getHours() > 11 ? 'PM': 'AM';
+          return (
+            <RouteLog dispatch={`${hoursDispatch}:${minutesDispatch} ${ampmDispatch}`}
+              eta={`${hours}:${minutes} ${ampm}`} type={route.type}
+              status={route.status} unitId={route.unitId} />
+          )
+        })}
+        <h1 className="route-status-header">Enroute</h1>
+        { tempRequested.map(route => {
+          const date = new Date(route.ETA)
+          const minutes = date.getMinutes();
+          const hours = date.getHours() % 12;
+          const ampm = date.getHours() > 11 ? 'PM': 'AM';
+          const dateDispatch = new Date(route.ETA)
+          const minutesDispatch = dateDispatch.getMinutes();
+          const hoursDispatch = dateDispatch.getHours() % 12;
+          const ampmDispatch = dateDispatch.getHours() > 11 ? 'PM': 'AM';
+          return (
+            <RouteLog dispatch={`${hoursDispatch}:${minutesDispatch} ${ampmDispatch}`}
+              eta={`${hours}:${minutes} ${ampm}`} type={route.type}
+              status={route.status} unitId={route.unitId} />
+          )
+        })}
+        <h1 className="route-status-header">On-Sight</h1>
+        { tempRequested.map(route => {
+          const date = new Date(route.ETA)
+          const minutes = date.getMinutes();
+          const hours = date.getHours() % 12;
+          const ampm = date.getHours() > 11 ? 'PM': 'AM';
+          const dateDispatch = new Date(route.ETA)
+          const minutesDispatch = dateDispatch.getMinutes();
+          const hoursDispatch = dateDispatch.getHours() % 12;
+          const ampmDispatch = dateDispatch.getHours() > 11 ? 'PM': 'AM';
+          return (
+            <RouteLog dispatch={`${hoursDispatch}:${minutesDispatch} ${ampmDispatch}`}
+              eta={`${hours}:${minutes} ${ampm}`} type={route.type}
+              status={route.status} unitId={route.unitId} />
+          )
+        })}
+      </div>
     </div>
   )
 }
