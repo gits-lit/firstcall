0e837de965dff73397765e867f2b6ca498cdf549
Jennifer <32044772+Froggerson@users.noreply.github.com>
2021-03-27 01:30:10 -0700

    stanley changed the design a bit

--- responder/src/components/DialHeader/Mute/index.jsx
+++ responder/src/components/DialHeader/Mute/index.jsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from 'react';
 import mute from '.../../../public/mute.svg'
 import pause from '.../../../public/pause.svg'
+import phone from '.../../../public/phone.svg'
 
 import './style.scss';
 
 const Mute = (props) => {
   return (
     <div className="mute">
-      <div className="icon-container">
+      <div className="mute-container">
         <img className="mute-icon" src={mute} alt="mute icon"/>
-        <img className="pause-icon" src={pause} alt="pause icon"/>
       </div>
+      <img className="pause-icon" src={pause} alt="pause icon"/>
+      <img className="phone-icon" src={phone} alt="phone icon"/>
+
     </div>
   )
 }
