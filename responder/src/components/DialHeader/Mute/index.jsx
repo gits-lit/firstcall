b919ea331d151cb3996af9da1a0041a05b6669b9
Jennifer <32044772+Froggerson@users.noreply.github.com>
2021-03-26 23:55:19 -0700

    Add files are other part of dial header

---
+++ responder/src/components/DialHeader/Mute/index.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+
+import './style.scss';
+
+const Mute = (props) => {
+  return (
+    <div className="mute">
+    </div>
+  )
+}
+
+export default Mute;
---
+++ responder/src/components/DialHeader/RecordingTime/index.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+
+import './style.scss';
+
+const RecordingTime = (props) => {
+  return (
+    <div className="recording-time">
+    </div>
+  )
+}
+
+export default RecordingTime;
