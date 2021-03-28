0a640b8a691898c850aa0524a828e261f19aad83
leestanley <stanleylee91776@gmail.com>
2021-03-28 01:13:30 -0700

    fixed main

--- caller/src/pages/MainPage.jsx
+++ caller/src/pages/MainPage.jsx
@@ -1,53 +1,47 @@
 import React, { useState } from 'react';
 
 import CameraPage from './CameraPage';
-import HelpETA from '../components/HelpETA'
-import Messages from '../components/Messages'
-import NavBar from '../components/NavBar'
-import SelfInfo from '../components/SelfInfo'
-import TopBar from '../components/TopBar'
+import HelpETA from '../components/HelpETA';
+import Messages from '../components/Messages';
+import NavBar from '../components/NavBar';
+import SelfInfo from '../components/SelfInfo';
+import TopBar from '../components/TopBar';
 
-import socketIOClient from "socket.io-client";
+import socketIOClient from 'socket.io-client';
 
 const ENDPOINT = 'https://firstcall-snu.herokuapp.com';
 const socket = socketIOClient(ENDPOINT);
-
 
 const MainPage = () => {
   const [click, setClick] = useState('message');
   return (
     <div>
-    <TopBar />
-    <NavBar click={click}
-      setClick={(clickInput) => {
-        setClick(clickInput);
-      }}
-    />
-    {
-      click === 'message' ?
+      <TopBar />
+      <NavBar
+        click={click}
+        setClick={(clickInput) => {
+          setClick(clickInput);
+        }}
+      />
+      {click === 'message' ? (
         <div>
           <Messages />
         </div>
-       : click === 'info' ? (
-        <SelfInfo socket={socket}/>
-      ) : click === 'help' ? (
-        <div>help</div>
+      ) : click === 'info' ? (
+        <SelfInfo socket={socket} />
       ) : click === 'camera' ? (
         <div>
           <CameraPage />
-        </div> 
+        </div>
       ) : click === 'help' ? (
         <div>
           <HelpETA />
         </div>
-      ) : click === 'camera' ?
-        <div>
-          camera
-        </div> :
-      <></>
-    }
-  </div>
-  )
-}
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
 
 export default MainPage;
