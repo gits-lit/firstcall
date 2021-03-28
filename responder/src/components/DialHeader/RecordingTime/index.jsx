9b10b19febadbf7f6523fb032b0491fc7f20ca6b
Joey Dang <jodang@ucsd.edu>
2021-03-28 05:00:46 -0700

    Add time adjustment

--- responder/src/components/DialHeader/RecordingTime/index.jsx
+++ responder/src/components/DialHeader/RecordingTime/index.jsx
@@ -7,6 +7,19 @@ const RecordingTime = (props) => {
   const [minutes, setMinutes] = useState("00");
   const [seconds, setSeconds] = useState("00");
 
+  useEffect(() => {
+    let min = Math.floor(props.seconds / 60);
+    let sec = Math.floor(props.seconds % 60);
+    if (min < 10) {
+      min = '0' + min.toString();
+    }
+    setMinutes(min);
+    if (sec < 10) {
+      sec = '0' + sec.toString();
+    }
+    setSeconds(sec);
+   }, [props.seconds])
+
   return (
     <div className="recording-time">
       <img className="recording-icon" src={recording} alt="recording icon"/>
