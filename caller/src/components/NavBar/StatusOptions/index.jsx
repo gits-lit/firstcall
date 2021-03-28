8a6ab0a2ee6b29229a878c9bd5ae83ce9764acee
Jennifer <32044772+Froggerson@users.noreply.github.com>
2021-03-27 17:48:10 -0700

    Add NavBar

--- caller/src/components/NavBar/StatusOptions/index.jsx
+++ caller/src/components/NavBar/StatusOptions/index.jsx
@@ -1,47 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import './style.scss';
 
 const StatusOptions = (props) => {
-  const [completedCount, setCompleted] = useState(0);
-  const [newCount, setNewCount] = useState(0);
-  const [ongoingCount, setOngoingCount] = useState(0);
-  
+  const [onsiteCount, setOnsite] = useState(0);
+  const [requestedCount, setRequestedCount] = useState(0);
+  const [enrouteCount, setEnrouteCount] = useState(0);
+
   useEffect(() => {
-    let completedStatus = 0;
-    let newStatus = 0;
-    let ongoingStatus = 0;
+    let onsiteStatus = 0;
+    let requestedStatus = 0;
+    let enrouteStatus = 0;
 
     for (let i = 0; i < props.data.length; i++) {
       if (props.data[i].status === '0') {
-        newStatus += 1;
+        requestedStatus += 1;
       } else if (props.data[i].status === '1' || props.data[i].status === '2') {
-        ongoingStatus += 1;
+        enrouteStatus += 1;
       } else {
-        completedStatus += 1;
+        onsiteStatus += 1;
       }
     }
-    setCompleted(completedStatus);
-    setNewCount(newStatus);
-    setOngoingCount(ongoingStatus);
+    setOnsite(onsiteStatus);
+    setRequestedCount(requestedStatus);
+    setEnrouteCount(enrouteStatus);
   }, [props.data]);
 
   return (
     <div className="status-options">
       <div className="option-num">
         <div className="option">
-          <div className="new-circle"></div>
-          <div className="option-description"><span>{newCount}</span> New </div>
+          <div className="requested-circle"></div>
+          <div className="option-description"><span>{requestedCount}</span> Requested </div>
         </div>
         <div className="option">
-          <div className="ongoing-circle"></div>
-          <div className="option-description"><span>{ongoingCount}</span> Ongoing </div>
+          <div className="enroute-circle"></div>
+          <div className="option-description"><span>{enrouteCount}</span> Enroute </div>
         </div>
         <div className="option">
-          <div className="completed-circle"></div>
-          <div className="option-description"><span>{completedCount}</span> Completed </div>
+          <div className="onsite-circle"></div>
+          <div className="option-description"><span>{onsiteCount}</span> OnSite </div>
         </div>
       </div>
+    </div>
   )
 }
 
-export default NavBar;
+export default StatusOptions;
--- caller/src/components/NavBar/index.jsx
+++ caller/src/components/NavBar/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import StatusOptions from './StatusOptions'
+import logo from '.../../../public/logo.svg'
+
 import './style.scss';
 
-const NavBar = () => {
+const NavBar = (props) => {
   return (
     <div className="nav-bar">
+      <img className="logo" src={logo} alt="logo pin"/>
+      <StatusOptions data={{}}/>
     </div>
   )
 }
