30596627b01186ba2843b8f714c5afc2f94811b9
Jeffrey Ha <jha@ucsd.edu>
2021-03-28 01:27:31 -0700

    bs

--- server/util/connections/chat.js
+++ server/util/connections/chat.js
@@ -1,11 +1,10 @@
 const system = require('../../util/system');
 const socket = require('../socket');
-const botName = 'Bot'; // idk
+// const botName = 'Bot'; // idk
 
 const moment = require('moment');
-const formatMessage = (name, text) => {
+const formatMessage = (text) => {
     return {
-        name,
         text,
         time: moment().format('h:mm a')
     };
@@ -13,43 +12,33 @@ const formatMessage = (name, text) => {
 
 module.exports = async (io, client) => {
     // only the user should call this, not responders
-    client.on('joinRoom', async uid => {
-        // get their username
-        let info = await system.getUser(uid);
-        if (!info.success) {
-            console.log(info.message);
-            client.disconnect();
-            return;
-        }
-
-        let username = (info.user.name || `User ${uid}`);
-        client.username = username;
+    client.on('joinRoom', uid => {
         client.uid = uid;
         client.joinedChat = true;
         client.join(uid);
 
         // let's welcome the user (feel free to change the welcome message)
-        client.emit('message', formatMessage(botName, `Welcome to FirstCall.`));
+        // client.emit('message', formatMessage(botName, `Welcome to FirstCall.`));
 
         // broadcast to other users
-        client.broadcast.to(uid).emit('message', formatMessage(botName, `${username} has connected.`));
+        // client.broadcast.to(uid).emit('message', formatMessage(botName, `${username} has connected.`));
     });
 
     client.on('chatMessage', data => {
         if (data.user_type === 'responder')
-            io.to(data.uid).emit('message', formatMessage(client.username, data.message));
+            io.to(data.uid).emit('message', formatMessage(data.message));
         else if (data.user_type === 'user')
-            io.to(client.uid).emit('message', formatMessage(client.username, data.message));
+            io.to('responders').emit('message', formatMessage(data.message));
     });
 
     client.on('imageSend', data => {
         if (data.user_type === 'responder')
-            io.to(data.uid).emit('image', formatMessage(client.username, data.image));
+            io.to(data.uid).emit('image', formatMessage(data.image));
         else
-            io.to(client.uid).emit('message', formatMessage(client.username, data.image));
+            io.to('responders').emit('message', formatMessage(data.image));
     });
 
-    client.on('disconnecting', data => {
+    /*client.on('disconnecting', data => {
         if (!client.joinedChat) return;
 
         // let the rooms know
@@ -57,5 +46,5 @@ module.exports = async (io, client) => {
             if (uid === 'responder') return;
             io.to(uid).emit('message', formatMessage(client.username, `${client.username} has disconnected.`));
         });
-    });
+    });*/
 };
